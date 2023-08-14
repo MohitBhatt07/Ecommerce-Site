e2e77d98dba5d2ed1bed2b3efbf176657121ae8f
Mohit Bhatt <bhattmohit869@gmail.com>
2023-08-14 20:15:25 +0530

    Added some Components

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {createBrowserRouter,RouterProvider} from "react-router-dom";
+import HomePage from './pages/Home/Home';
+import ProductPage from './pages/Product/Product';
+import Products from './pages/Products/Products';
+
+const router = createBrowserRouter([
+  {path :'/',element : <HomePage/>},
+  {path : '/products/:id',element : <Products/>},
+  {path : '/product/:id',element : <ProductPage/>}
+])
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router}/>
 }
 
 export default App;
