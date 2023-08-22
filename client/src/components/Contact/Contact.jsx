7356ca9996819e93561455881adaf180dd14eb15
Mohit Bhatt <bhattmohit869@gmail.com>
2023-08-22 09:10:59 +0530

    added main page components

---
+++ client/src/components/Contact/Contact.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Contact.scss';
+import {BsFacebook} from 'react-icons/bs';
+import {BsTwitter} from 'react-icons/bs';
+import {AiOutlineInstagram} from 'react-icons/ai';
+import {FaPinterest} from 'react-icons/fa';
+
+function Contact() {
+  return (
+    <div className='contact'>
+      <div className="wrapper">
+        <span>Be with us</span>
+        <div className="mail">
+          <input type= 'email' placeholder='Enter your email'/>
+          <button>Join us</button>
+        </div>
+      </div>
+      <div className="icons"></div>
+        <BsFacebook/>
+        <AiOutlineInstagram/>
+        <BsTwitter/>
+        <FaPinterest/>
+    </div>
+  )
+}
+
+export default Contact;
