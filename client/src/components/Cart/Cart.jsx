e7f3dcb4b3ce87102dfa3c5289bea548326d0396
Mohit Bhatt <bhattmohit869@gmail.com>
2023-08-24 19:58:13 +0530

    product and slider changes

---
+++ client/src/components/Cart/Cart.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Cart() {
+  return (
+    <div className='cart'>
+      <h1>Items in cart</h1>
+      {
+        data.map  
+      }
+    </div>
+  )
+}
+
+export default Cart
