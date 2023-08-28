5359d5e6c2b6528749a5341766335e424f4babff
Mohit Bhatt <bhattmohit869@gmail.com>
2023-08-28 19:47:09 +0530

    Fetched data from strapi

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import './Card.scss';
-function Card({item}) {
+function  Card({item}) {
   return (
     <Link className='link' to={`/product/${item.id}`}>
       <div className='card'>
         <div className = 'image'>
           {item.isNew && <span>New Season</span>}
-          <img src = {item.img} className='main-image' alt=''></img>
-          <img src = {item.img2} className='second-image' alt=''/>
+          <img src = {process.env.REACT_APP_UPLOAD_URL + item.img.data.attributes.url} className='main-image' alt=''></img>
+          <img src = {process.env.REACT_APP_UPLOAD_URL +item.img2.data.attributes.url } className='second-image' alt=''/>
         </div>
         <h2>{item.title}</h2>
         <div className='prices'>
-          <h3>${item.oldPrice}</h3>
-          <h3>${item.price}</h3>
+          <h3>₹{item.price + 400}</h3>
+          <h3>₹{item.price}</h3>
           
         </div>
       </div>
--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,49 +1,66 @@
-import React from "react";
+import React, { useEffect ,useState} from "react";
 import Card from "../Card/Card";
 import "./FeaturedProducts.scss";
-
+import axios from 'axios';
 function FeaturedProducts({ type }) {
-  const data = [
-    {
-      id: 1,
-      img: "https://m.media-amazon.com/images/I/613KABeTdwL._UY741_.jpg",
-      img2: "https://m.media-amazon.com/images/I/51OVMS7p9CL._UY741_.jpg",
-      title: "Cotton blend half sleeves T-shirt",
-      isNew: true,
-      oldPrice: 1099,
-      price: 294,
-    },
-    {
-      id: 2,
-      img:"https://m.media-amazon.com/images/I/61n4DmCEzvL._UY741_.jpg" ,
-      img2: "https://m.media-amazon.com/images/I/71Z9fNLQ55L._UY741_.jpg",
-      title: "Cotton Hooded Cardigan",
-      isNew: true,
-      oldPrice: 1899,
-      price: 850,
-    },
-    {
-      id: 3,
-      img: "https://m.media-amazon.com/images/I/41TRg1JUtwL._UY679_.jpg",
-      img2: "https://m.media-amazon.com/images/I/51KTx9vKNTL._UY679_.jpg",
-      title: "Long Puff Sleeve",
-      isNew: false,
-      oldPrice: 1799,
-      price: 950,
-    },
-    {
-      id: 4,
-      img: "https://m.media-amazon.com/images/I/718qNWe7F0L._UY741_.jpg",
-      img2: "https://m.media-amazon.com/images/I/61ACT9Zwg3L._UY741_.jpg",
-      title: "A-Line Knee Length Bodycon",
-      isNew: false,
-      oldPrice: 1399,
-      price: 750,
-    },
+  // const data = [
+  //   {
+  //     id: 1,
+  //     img: "https://m.media-amazon.com/images/I/613KABeTdwL._UY741_.jpg",
+  //     img2: "https://m.media-amazon.com/images/I/51OVMS7p9CL._UY741_.jpg",
+  //     title: "Cotton blend half sleeves T-shirt",
+  //     isNew: true,
+  //     oldPrice: 1099,
+  //     price: 294,
+  //   },
+  //   {
+  //     id: 2,
+  //     img:"https://m.media-amazon.com/images/I/61n4DmCEzvL._UY741_.jpg" ,
+  //     img2: "https://m.media-amazon.com/images/I/71Z9fNLQ55L._UY741_.jpg",
+  //     title: "Cotton Hooded Cardigan",
+  //     isNew: true,
+  //     oldPrice: 1899,
+  //     price: 850,
+  //   },
+  //   {
+  //     id: 3,
+  //     img: "https://m.media-amazon.com/images/I/41TRg1JUtwL._UY679_.jpg",
+  //     img2: "https://m.media-amazon.com/images/I/51KTx9vKNTL._UY679_.jpg",
+  //     title: "Long Puff Sleeve",
+  //     isNew: false,
+  //     oldPrice: 1799,
+  //     price: 950,
+  //   },
+  //   {
+  //     id: 4,
+  //     img: "https://m.media-amazon.com/images/I/718qNWe7F0L._UY741_.jpg",
+  //     img2: "https://m.media-amazon.com/images/I/61ACT9Zwg3L._UY741_.jpg",
+  //     title: "A-Line Knee Length Bodycon",
+  //     isNew: false,
+  //     oldPrice: 1399,
+  //     price: 750,
+  //   },
     
-  ];
+  // ];
   const [products,setProducts]= useState([]);
 
+  useEffect(()=>{
+    const fetchData = async ()=>{
+      try{
+        const response = await axios.get(process.env.REACT_APP_API_URL +`/products?populate=*&[filters][type][$eq]=${type}` ,{
+           headers: {
+              Authorization : "bearer "+ process.env.REACT_APP_API_TOKEN2,
+           }
+        });
+        console.log(response);
+        setProducts(response.data.data);
+      }
+      catch(err){
+        console.log(err);
+      }
+    };
+    fetchData();
+  },[type]);
   
   return (
     <div className="featuredProducts">
@@ -60,8 +77,8 @@ function FeaturedProducts({ type }) {
       </div>
 
       <div className="bottom">
-        {data.map((item) => (
-          <Card key={item.id} item={item} className='myCard'/>
+        {products.map((item) => (
+          <Card key={item.id} item={item.attributes} className='myCard'/>
         ))}
       </div>
     </div>
